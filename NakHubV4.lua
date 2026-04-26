b1ee946113723c9c14926680054d7a4d25b7becd
Naknohack <kiet41180@gmail.com>
2026-04-26 11:03:27 +0700

    Delete NakHubV4.lua

--- NakHubV4.lua
+++
@@ -1,8 +0,0 @@
--- Chạy script chính
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Naknohack/NakHubBF/refs/heads/main/3176434055923165.lua"))()
-
--- Đợi 2 giây
-task.wait(2)
-
--- Chạy Fast Attack
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Naknohack/NakHubBF/refs/heads/main/Fast%20Attack.lua"))()
